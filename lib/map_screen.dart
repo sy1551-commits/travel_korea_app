43da104985d916b6aeee2ac085d70e6d498a68d5
sugom <sy1551@gmail.com>
2025-11-02 19:38:53 +0900

    map에 도시표시(색상포함)

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+const List<String?> cityList = [
+  null,
+  "강릉",
+  "서울",
+  "춘천",
+  "인천",
+  "안동",
+  "전주",
+  "경주",
+  "나주",
+  "부산",
+  null,
+  null,
+  "제주",
+];
+
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
@@ -28,6 +44,40 @@ class _MapScreenState extends State<MapScreen> {
                   fit: BoxFit.cover,
                   image: AssetImage("assets/images/map.png"),
                 ),
+              ),
+              child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 5.0,
+                  crossAxisSpacing: 45.0,
+                ),
+                itemBuilder: (context, index) => cityList[index] != null
+                    ? InkWell(
+                        onTap: () {},
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: [
+                              const Color(0x77F44336),
+                              const Color(0x77FFEB3B),
+                              const Color(0x772196F3),
+                              const Color(0x774CAF50),
+                              const Color(0x779C27B0),
+                            ][index % 5],
+                          ),
+                          child: Center(
+                            child: Text(
+                              cityList[index]!,
+                              style: const TextStyle(fontSize: 20.0),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Container(),
+                itemCount: cityList.length,
               ),
             ),
           ],
