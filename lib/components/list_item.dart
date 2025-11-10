b932b4b54ca1c78cb68e00fd0648116ba6ab5d03
sugom <sy1551@gmail.com>
2025-11-10 21:05:04 +0900

    구글맵추가(위경도포함)

--- lib/components/list_item.dart
+++ lib/components/list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ListItem extends StatelessWidget {
   const ListItem({super.key, required this.data});
@@ -6,6 +7,16 @@ class ListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Set<Marker> markers = <Marker>{
+      Marker(
+        markerId: MarkerId(data["name"] ?? ""),
+        position: LatLng(data["lat"] ?? 37.495361, data["logt"] ?? 127.033079),
+        infoWindow: InfoWindow(
+          title: data["name"] ?? "",
+          snippet: data["address"] ?? "",
+        ),
+      ),
+    };
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Column(
@@ -28,15 +39,14 @@ class ListItem extends StatelessWidget {
               ),
             ],
           ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 10.0)),
           Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width - 140.0,
-                child: Text(
-                  "${data["description"] ?? ""}\n위치: ${data["address"] ?? ""}",
-                ),
+                child: Text(data["description"] ?? ""),
               ),
               SizedBox(
                 width: 100.0,
@@ -47,6 +57,35 @@ class ListItem extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 10.0)),
+          SizedBox(
+            height: 200.0,
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  data["lat"] ?? 37.495361,
+                  data["logt"] ?? 127.033079,
+                ),
+                zoom: 15.0,
+              ),
+              markers: markers,
+              compassEnabled: false,
+              rotateGesturesEnabled: false,
+              scrollGesturesEnabled: false,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: true,
+              buildingsEnabled: true,
+              myLocationButtonEnabled: false,
+              onMapCreated: (controller) {
+                // 마커가 항상 열려 있도록 설정
+                // ignore: avoid_function_literals_in_foreach_calls
+                markers.forEach((marker) {
+                  controller.showMarkerInfoWindow(marker.markerId);
+                });
+              },
+            ),
           ),
         ],
       ),
